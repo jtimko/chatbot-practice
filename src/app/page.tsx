5eb3613bd81feda75e79442cd6ca6ff6a5abb25f
jtimko <justin@jtimko.com>
2023-05-08 10:34:07 -0700

    Cleaned up the style and removed user-agent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,22 +7,21 @@ export default function Home() {
   const [response, setResponse] = useState<string>("");
 
   async function chatgpt() {
-
-    console.log(JSON.stringify(response));
     const data = await fetch(`http://localhost:3000/api/chat`, {
       method: 'POST',
       body: JSON.stringify({
         data: response
       }),
-    }).then((res) => res.json()).then((data) => { console.log("some data: " + JSON.stringify(data)); setAnswer(data.gpt) });
+    }).then((res) => res.json()).then((data) => { setAnswer(data.gpt); });
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center p-24">
         <input
           type="text"
           name="content"
           onChange={(e) => setResponse(e.target.value)}
+          style={{ padding: "10px"}}
           />
         <button onClick={() => chatgpt()}>Submit</button>
      {answer && <p>{answer}</p>}
